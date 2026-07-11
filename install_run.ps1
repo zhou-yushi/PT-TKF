f8756690b145195939624adcabc03d30369a31e4
Doc.Zhou <13621977041@163.com>
2026-07-11 19:53:45 +0800

    chore: 移除诊断临时文件

--- install_run.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Continue"
-python -m pip install playwright 2>&1 | Out-File -Append pip.log
-python -m playwright install chromium 2>&1 | Out-File -Append pip.log
-python screenshot.py 2>&1 | Out-File -Append shot3.log
-Write-Host "DONE" | Out-File -Append shot3.log
